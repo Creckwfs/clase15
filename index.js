18a8cfab7374415c308d980b3a6451e1fc0a020f
alexdelamora <creckwfs@gmail.com>
2019-09-23 20:34:24 -0500

    probando procesos

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Memoria");
+console.log(process.memoryUsage());
+console.log('Cpu usage');
+console.log(process.cpuUsage());
+console.log('Variables')
+console.log(process.allowedNodeEnvironmentFlags
+console.log("Directorio de ejecucion")
+console.log(process.cwd())
